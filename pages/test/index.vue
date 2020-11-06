26044eeb5723f4766c73a94e0cf57344c07cb813
kagehiro <tokuda.yasuyuki@cloud-ace.jp>
2020-11-06 18:50:44 +0900

    最初のコミット

---
+++ pages/test/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <v-app>
+            <v-content>
+                <v-container fill-height>
+                    aaa<br>
+                    aaa<br>
+                    aaa<br>
+                </v-container>
+            </v-content>
+        </v-app>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+</script>
